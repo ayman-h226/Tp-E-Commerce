6b9f32a5d6c8a5ba22b8bc202a1fbb4bca8297ed
ayman-h226 <doulkom.ayman@gmail.com>
2024-04-12 16:29:43 +0200

    Refactore

---
+++ config-file-server/src/test/java/com/fisa/configfileserver/ConfigFileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisa.configfileserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigFileServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
